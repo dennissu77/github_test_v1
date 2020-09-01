a23a4db8a3b634830cf033332c14615d00e9e42a
dennissu77 <57853693+dennissu77@users.noreply.github.com>
2020-09-01 15:20:39 +0800

    1

---
+++ build/precache-manifest.becc372dccf6baa89597fdbfd5f1943a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2e675c2ce51e791e44c46e41f8f4e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96e6def36cc2a03b245c",
+    "url": "/static/js/2.768c2243.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.768c2243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ae7ed424e5cea980e4",
+    "url": "/static/js/main.2dc31b3a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
